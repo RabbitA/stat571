a0d96d1a7042bfd5cc7d804065f0e58b382026fe
Ken Youens-Clark <kyclark@gmail.com>
2016-03-04 15:28:07 -0700

    new stuff

--- hw03/4.19.R
+++ hw03/4.19.R
@@ -9,5 +9,36 @@ matrix(df$response, 4, 4)
 amod = aov(response ~ tip + coupon, data=df)
 summary(amod)
 
+plot(residuals(lm(response ~ tip + coupon, data=df)))
+abline(0, 0, col='blue')
+
+plot(residuals(lm(response ~ coupon, data=df)))
+plot(df$tip, df$response, type="p")
+
 # Fisher LSD
 library(agricolae)
+lsd1 = LSD.test(amod, "tip")
+lsd2 = LSD.test(amod, "coupon")
+df$ab <- with(df, interaction(df$tip, df$coupon)) 
+comp3 <- HSD.test(,"ab") 
+
+
+require(multcomp) 
+tuk <- glht(amod, linfct = mcp(tip="Tukey"))
+summary(tuk)
+tuk.cld <- cld(tuk)
+plot(tuk.cld)
+
+df$ab <- with(df, interaction(df$tip, df$coupon)) 
+xi.aov <- aov(response ~ ab, data=df) 
+summary(xi.aov) 
+xi.glht <- glht(xi.aov, linfct=mcp(ab="Tukey")) 
+xi.cld <- cld(tuk)
+plot(xi.cld)
+confint(xi.glht)
+
+## graphs 
+## boxplots 
+require(lattice) 
+bwplot(response ~ ab, data=df)
+plot(xi.glht)
