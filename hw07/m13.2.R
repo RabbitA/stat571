94ed0824375f0016f9ae488bb7863a3a753697b9
Ken Youens-Clark <kyclark@gmail.com>
2016-04-25 11:09:43 -0700

    homework 7

--- hw07/m13.2.R
+++ hw07/m13.2.R
@@ -1,23 +1,32 @@
 library(lme4)
-library(lattice)
-
 dat = read.csv("~/work/stat571/hw07/13.2.csv")
 dat$part = factor(dat$part)
 dat$inspector = factor(dat$inspector)
-dat$test = factor(dat$test)
+amod = lmer(y ~ inspector * (1|part), dat)
+options(show.signif.stars=FALSE)
+summary(amod)
+plot(amod)
+coef(amod)
 
+
+dat.lmer = lmer(y ~ (1|inspector) + (1|part), dat)
+summary(dat.lmer)
+lsmeans(dat.lmer)
+
+coef(dat.lmer)
+plot(dat.lmer)
+
+plot(fitted(dat.lmer), resid(dat.lmer))
+abline(h=0, col="red")
+
+#library(lattice)
+#dat.lmer = lmer(y ~ (inspector|part), dat)
 # old.par = par(mfcol=c(2,1))
 # par(old.par)
+#boxplot(y ~ inspector*part, data=dat, col=c("white","lightgray"),politeness)
 
 # much variation by part
-xyplot(y ~ part, dat, main="Part")
-
+#xyplot(y ~ part, dat, main="Part")
 # not much variation by inspector
-xyplot(y ~ inspector, dat, main="Inspector")
-
-boxplot(y ~ inspector*part, data=dat, col=c("white","lightgray"),politeness)
-
-dat.lmer = lmer(y ~ 1 + (1|inspector) + (1|part), dat)
+#xyplot(y ~ inspector, dat, main="Inspector")
 #dat.lmer = lmer(y ~ 1 + (1|inspector) + (1|part) + (1+inspector|part), dat)
-summary(dat.lmer)
-coef(dat.lmer)
