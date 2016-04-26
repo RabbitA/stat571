9b90490c7e2983eb301c286429688e6809ba4837
Ken Youens-Clark <kyclark@gmail.com>
2016-04-25 20:40:18 -0700

    boxplots

--- project/popcorn.R
+++ project/popcorn.R
@@ -3,13 +3,17 @@ unpopped = c(332,338,320,389,398,411,88,113,88,277,268,283,379,381,
              394,84,100,127,262,295,288,388,371,369,248,86,108)
 additive = c(rep("none", 9), rep("olive", 9), rep("butter", 9))
 power = rep(c(rep("900", 3), rep("1000", 3), rep("1200", 3)), 3)
-dat = data.frame(additives, power, unpopped)
-mod = aov(unpopped ~ additive * power, dat)
+
+dat = data.frame(additive, power, unpopped)
+mod = aov(unpopped ~ additive + power, dat)
 options(show.signif.stars=FALSE)
 summary(mod)
 
 old.par = par(mfcol = c(1,2))
+boxplot(unpopped ~ additive, dat, main="Additive")
+boxplot(unpopped ~ power, dat, main="Wattage")
 plot(mod)
 par(old.par)
+
 
 
